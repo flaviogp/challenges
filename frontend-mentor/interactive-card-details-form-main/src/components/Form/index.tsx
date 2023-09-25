f43c90313f7fbd1ea8bbeeab06112dbf4168691e
FlavioGP <flavio.gpinheiro@hotmail.com>
2023-09-25 11:06:19 -0300

    finalizando estilização

--- frontend-mentor/interactive-card-details-form-main/src/components/Form/index.tsx
+++ frontend-mentor/interactive-card-details-form-main/src/components/Form/index.tsx
@@ -1,6 +1,17 @@
+import { useState } from "react";
 
+export interface FormData{
+    cardName: string;
+    cardNumber: string;
+    expDate: {
+        month: string;
+        year: string;
+    }
+    cvc: string
+}
 
 export default function Form () {
+    const [formData, setFormData] = useState()
     return(
         <form action="#" className="form">
             <label htmlFor="name">
